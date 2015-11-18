49ad4826048d92fd9c9caba89d9cd8a27cc816a2
cerjio <cerjio@gmail.com>
2015-11-18 14:09:13 -0600

    Agregando proyecto de hamburguesas, ya medio hambre...

---
+++ Hamburguesas/Hamburguesas/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Hamburguesas
+//
+//  Created by cerjio on 18/11/15.
+//  Copyright © 2015 cerjio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pais: UILabel!
+    @IBOutlet weak var hamburguesa: UILabel!
+    let paises = ColeccionDePaises()
+    let hamburguesas = ColeccionDeHamburguesas()
+    let colores = Colores()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func obtenerPaisYHamburguesa() {
+        pais.text = paises.obtenPais()
+        hamburguesa.text = hamburguesas.obtenHamburguesa()
+        let colorAleatorio = colores.regresaColorAleatorio()
+        view.backgroundColor = colorAleatorio
+        view.tintColor = colorAleatorio
+        
+    }
+}
+
---
+++ MasFeliz/MasFeliz/ColoresStruct.swift
@@ -0,0 +1,9 @@
+//
+//  ColoresStruct.swift
+//  MasFeliz
+//
+//  Created by cerjio on 16/11/15.
+//  Copyright © 2015 cerjio. All rights reserved.
+//
+
+import Foundation
